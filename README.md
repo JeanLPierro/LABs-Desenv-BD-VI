80a473b633ac80c34c8e70e1d4a8e433291f6ff2
JeanPierro <jeanpierro08@gmail.com>
2020-09-18 17:06:55 -0300

    Criação Testes [estado vermelho]

---
+++ README.md
@@ -0,0 +1,9 @@
+# LABORATÓRIO DE DESENVOLVIMENTO EM BANCO DE DADOS VI
+
+**Nome**: Jean Lucca Pierro Santos
+
+**RA**: 1460281813020
+
+# Pastas de Projetos
+
+**entrega_introdução**: Trabalho que ilustra o método de desenvolvimento orientado a testes (TDD).
---
+++ entrega-introducao/src/test/java/SorteioTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SorteioTest {
+
+    @Test
+    public void deveRetornarRespostaPositiva(){
+        String resp = "";
+
+        Assert.assertEquals("Sim", resp);
+    }
+
+    @Test
+    public void deveRetornarRespostaNegativa(){
+        String resp = "";
+
+        Assert.assertEquals("Nao", resp);
+    }
+}
